d395e57ded949b99b64f2e781c4f76569ce4f465
Marvin - The Flint <Marv.Murmann@gmx.net>
2019-03-15 11:29:27 +0100

    .

--- site/themes/flint/js/src/menu.js
+++ site/themes/flint/js/src/menu.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 //Back button
 $(document).ready(function(){
     $(".reset").click(function(){
-        $(".t2").css({"opacity" : "0", "transform" : "scale(1.5)"}).delay(1000).slideUp(500, function(){
+        $(".t2").css({"opacity" : "0", "transform" : "scale(1.4)"}).delay(1000).slideUp(500, function(){
             $(".t1").slideDown(500, function(){
                 $(".t1").css({
                     "opacity" : "1",
@@ -22,16 +22,26 @@ $(document).ready(function(){
 //Panel switches
 $(document).ready(function(){
     $(".t1").click(function(){
-        $(this).attr("style", "transform: scale(1.5) !important");
+        $(this).attr("style", "transform: scale(1.4) !important");
         var ClickedEleId = $(this).attr("id");
         $(".t1").css("opacity", "0").delay(1000).slideUp(500, function(){
             $("." + ClickedEleId).slideDown(500, function(){
                 $("." + ClickedEleId).css({
                     "opacity" : "1",
-                    "transform" : "scale(1)"
+                    "transform" : "scale(1)",
+                    "display" : "flex"
                 });
             })
         })
     })
 })
+$(function(){
+    // Bind the swipeHandler callback function to the swipe event on div.box
+    $( ".switch-div" ).on( "swipe", swipeHandler );
+   
+    // Callback function references the event target and adds the 'swipe' class to it
+    function swipeHandler( event ){
+      $( event.target ).addClass( "swipe" );
+    }
+  });
 
